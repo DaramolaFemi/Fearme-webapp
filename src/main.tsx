865cefbaea003bdff1e210648a9919737d4befc7
Daramola Femi() <68006992+DaramolaFemi@users.noreply.github.com>
2026-09-13 00:10:40 +0100

    Fix mobile CTA styling
    Restore the mobile poetry and project CTA treatment without changing desktop layout.

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "@fontsource/dm-sans/latin-600.css";
 import "@fontsource/instrument-serif/latin-400.css";
 import "@fontsource/instrument-serif/latin-400-italic.css";
 import "./style.css";
+import "./mobile-cta-fixes.css";
 import SitePage from "./components/SitePage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
